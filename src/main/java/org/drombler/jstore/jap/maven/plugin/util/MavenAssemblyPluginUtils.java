65a23a9bd6051fc0ff73e6b129be840ba9197396
Florian Brunner <fbrunnerreg@gmx.ch>
2018-06-21 00:14:20 +0200

    jackson version

--- src/main/java/org/drombler/jstore/jap/maven/plugin/util/MavenAssemblyPluginUtils.java
+++ src/main/java/org/drombler/jstore/jap/maven/plugin/util/MavenAssemblyPluginUtils.java
@@ -6,4 +6,5 @@ package org.drombler.jstore.jap.maven.plugin.util;
 public final class MavenAssemblyPluginUtils {
 
     public static final PluginProperty ATTACH_PROPERTY = new PluginProperty("attach", "assembly.attach");
+    public static final PluginProperty DESCRIPTORS_PROPERTY = new PluginProperty("descriptors", null);
 }
